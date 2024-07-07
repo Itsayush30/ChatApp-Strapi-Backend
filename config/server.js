b0636e8f84d65c01248d5da145824aa09bc958d2
itsayush30 <itsayush30@gmail.com>
2024-07-07 14:10:01 +0530

    Updated server/config

--- config/server.js
+++ config/server.js
@@ -2,7 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+        'SltJaHds6HFNXjl5jxGl/Q==',
+        'GPgKNe6SLiLqS74M0Ms3vA==',
+        'Zp5Ghy+EIwrlW2Dt4QLHqA==',
+        'ilBAJo2B35R1CI5t/kdYlQ=='
+      ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
